2f4452262edffbd944f1a10d7567f2ddb326b5a6
lichenbuliren <617106268@qq.com>
2019-05-04 15:52:25 +0800

    ✨  Counter 第二版

--- src/Counter.js
+++ src/Counter.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 const Counter = () => {
   const [count, setCount] = useState(0)
 
+  const handleAlertClick = () => {
+    setTimeout(() => {
+      alert(`Yout clicked me: ${count}`)
+    }, 3000)
+  }
+
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={handleAlertClick}>Show alert</button>
     </div>
   )
 }
