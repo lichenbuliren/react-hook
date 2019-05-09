da78f52c014498530e2531b141c62444270e9ef0
lichenbuliren <617106268@qq.com>
2019-05-09 10:50:07 +0800

    refactor(GoodsListTable): 优化表格

--- src/GoodsListTable.js
+++ src/GoodsListTable.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
-import { Table, Avatar } from 'antd'
-import { fetchGoodsList } from './services'
-import { useService } from './hooks'
+import React, { useState } from "react";
+import { Table, Avatar } from "antd";
+import { fetchGoodsList } from "./services";
+import { useService } from "./hooks";
 
 const columns = [
   {
-    title: '商品名称',
-    key: 'name',
-    dataIndex: 'name',
+    title: "商品名称",
+    key: "name",
+    dataIndex: "name",
     render: (name, row) => (
       <React.Fragment>
         <Avatar src={row.img} />
@@ -17,37 +17,37 @@ const columns = [
     )
   },
   {
-    title: '商品价格',
-    key: 'price',
-    dataIndex: 'price'
+    title: "商品价格",
+    key: "price",
+    dataIndex: "price",
+    render: price => `¥ ${parseFloat(price).toFixed(2)}`
   },
   {
-    title: '商品库存',
-    key: 'stock',
-    dataIndex: 'stock'
+    title: "商品库存",
+    key: "stock",
+    dataIndex: "stock"
   },
   {
-    title: '创建时间',
-    key: 'createTime',
-    dataIndex: 'createTime'
+    title: "创建时间",
+    key: "createTime",
+    dataIndex: "createTime"
   },
   {
-    title: '更新时间',
-    key: 'updateTime',
-    dataIndex: 'updateTime'
+    title: "更新时间",
+    key: "updateTime",
+    dataIndex: "updateTime"
   }
-]
+];
 
 const GoodsListTable = () => {
-  const [pageSize, setPageSize] = useState(10)
-  const [pageNo, setPageNo] = useState(1)
+  const [pageSize, setPageSize] = useState(5);
+  const [pageNo, setPageNo] = useState(1);
   const { loading = false, error, response = {} } = useService(fetchGoodsList, {
     pageSize,
     pageNo
-  })
+  });
 
-  const { list = [] } = response || {}
-  console.log('response', response)
+  const { list = [], total } = response || {};
 
   return (
     <div>
@@ -59,18 +59,15 @@ const GoodsListTable = () => {
         pagination={{
           pageSize,
           current: pageNo,
+          total,
           onChange: (pageNo, pageSize) => {
-            setPageNo(pageNo)
-            setPageSize(pageSize)
+            setPageNo(pageNo);
+            setPageSize(pageSize);
           }
-        }}
-        style={{
-          width: '80%',
-          margin: '0 auto'
         }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default GoodsListTable
+export default GoodsListTable;
